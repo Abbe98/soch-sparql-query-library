f36cd90cd494b0773f5ee8398dcf5df665bbddc6
Abbe98 <albin.post@gmail.com>
2019-05-02 20:14:09 +0200

    added JSON generation

---
+++ generate_json.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+queries = list()
+for filename in os.listdir('queries'):
+    with open('queries/' + filename, 'r') as f:
+        title, tags, body = f.read().split('\n', 2)
+        title = title.replace('#title: ', '')
+        tags = tags.replace('#tags: ', '').split(',')
+        body = body.replace('\n', '', 1)
+
+        query = {}
+        query['tilte'] = title
+        query['tags'] = tags
+        query['body'] = body
+        
+        queries.append(query)
+
+with open('dist/queries.json', 'w') as outfile:
+    json.dump(queries, outfile)
+
+print('Done generating JSON')
